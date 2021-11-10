2ad2edf3bccbac27c746c8e5fa25614fbe7e416f
huangpan <you@example.com>
2021-11-10 08:24:08 +0800

    md5

---
+++ realworld-api-express/util/md5.js
@@ -0,0 +1,11 @@
+const crypto = require('crypto') // node 原生模块
+// 获取cryto 支持的散列算法
+console.log(crypto.getHashes())
+
+
+
+module.exports = str => {
+  return crypto.createHash('md5')
+  .update('dshsjveruyruv'+str) // 需要加密的铭文
+  .digest('hex') // 转换为十六进制
+}
